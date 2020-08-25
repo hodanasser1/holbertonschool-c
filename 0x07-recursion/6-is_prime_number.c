a8a4671f898e622cd5c50cb61ae7414a99a5c37a
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-25 19:06:09 +0000

    6

---
+++ 0x07-recursion/6-is_prime_number.c
@@ -0,0 +1,42 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - print string recursive
+ * @n: int
+ *
+ * Return: Always 0.
+ */
+int is_prime_number(int n)
+{
+	if (n < 2)
+	{
+		return (0);
+	}
+
+	return (checkForPrime(n, n / 2));
+}
+
+/**
+ * checkForPrime - print string recursive
+ * @n: int
+ * @i: int
+ *
+ * Return: Always 0.
+ */
+int checkForPrime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	else if (n % i == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		i = i - 1;
+		return checkForPrime(n, i);
+	}
+}
+
