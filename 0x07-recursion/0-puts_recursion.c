7d9d3582c96df3a3d1583edbcf19ae59952ea8d0
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-25 16:16:01 +0000

    0

---
+++ 0x07-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - print string recursive
+ * @s: String to print
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
