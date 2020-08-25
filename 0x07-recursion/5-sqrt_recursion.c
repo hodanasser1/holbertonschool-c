1d6a2a6fe471caa143b6f0cd395e48fb50ef7245
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-25 21:15:09 +0000

    6

---
+++ 0x07-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "holberton.h"
+
+/**
+ * _sqrt_recursion - print string recursive
+ * @x: int
+ *
+ * Return: Always 0.
+ */
+int _sqrt_recursion(int x)
+{
+	if (x < 0)
+	{
+		return (-1);
+	}
+	return (sqrtSearch(0, x, x));
+}
+
+/**
+ * sqrtSearch - print string recursive
+ * @low: int
+ * @high: int
+ * @num: int
+ *
+ * Return: Always 0.
+ */
+
+int sqrtSearch(int low, int high, int num)
+{
+	if (low <= high)
+	{
+		int mid = (low + high) / 2;
+
+		if (mid * mid == num)
+		{
+			return (mid);
+		}
+		else if (mid * mid < num)
+		{
+			return (sqrtSearch(mid + 1, high, num));
+		}
+		else
+		{
+			return (sqrtSearch(low, mid - 1, num));
+		}
+	}
+	return (-1);
+}
--- 0x07-recursion/holberton.h
+++ 0x07-recursion/holberton.h
@@ -8,6 +8,7 @@ int _putchar(char c);
 /* int factorial(int n);*/
 /* int _pow_recursion(int x, int y);*/
 int _sqrt_recursion(int n);
+int sqrtSearch(int low, int high, int N);
 int is_prime_number(int n);
 int checkForPrime(int n, int i);
 
