588b97d43409884a13d0efd47544a0ff96f45e06
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-25 18:28:24 +0000

    4

---
+++ 0x07-recursion/4-pow_recursion.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _pow_recursion - print string recursive
+ * @x: int
+ * @y: int
+ *
+ * Return: Always 0.
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	return (_pow_recursion(x, y - 1) * x);
+}
+
