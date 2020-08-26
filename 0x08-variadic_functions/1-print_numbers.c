2da1242719ff707d3be479b5b7d8eb1dcda02a1f
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-26 17:42:17 +0000

    1

---
+++ 0x08-variadic_functions/1-print_numbers.c
@@ -0,0 +1,72 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * pr_int - print string recursive
+ * @n: String to print
+ *
+ * Return: Always 0.
+ */
+void pr_int(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n / 10 != 0)
+		pr_int(n / 10);
+	_putchar((n % 10) + '0');
+}
+
+/**
+ * _puts_recursion - print string recursive
+ * @s: String to print
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
+
+
+/**
+ * print_numbers - check the code for Holberton School students.
+ * @separator: int
+ * @num: int
+ * @...: valist
+ *
+ * Return: Always 0.
+ */
+void print_numbers(const char *separator, const unsigned int num, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	/* initialize valist for num number of arguments */
+	va_start(valist, num);
+
+	/* access all the arguments assigned to valist */
+	for (i = 0; i < num; i++)
+	{
+		pr_int((int)va_arg(valist, int));
+		if ((i != num - 1) && separator != NULL)
+		{
+			_puts_recursion((char *)separator);
+		}
+		if (i == num - 1)
+		{
+			_putchar((char)'\n');
+		}
+	}
+
+	/* clean memory reserved for valist */
+	va_end(valist);
+}
--- 0x08-variadic_functions/variadic_functions.h
+++ 0x08-variadic_functions/variadic_functions.h
@@ -1,6 +1,10 @@
 #ifndef VARIADIC
 #define VARIADIC
 
+void pr_int(int n);
+int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
+void _puts_recursion(char *s);
+void print_numbers(const char *separator, const unsigned int n, ...);
 
 #endif
