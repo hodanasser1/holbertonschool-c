81aeb5c1b2f9ffac274ed72d414c19d005217c5d
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-26 18:00:36 +0000

    2

---
+++ 0x08-variadic_functions/2-print_strings.c
@@ -0,0 +1,64 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * _puts_recursion - print string recursive
+ * @s: String to print
+ *
+ * Return: Always 0.
+ */
+void _puts_recursion(char *s)
+{
+	if (s[0] == '\0')
+	{
+		return;
+	}
+	_putchar(s[0]);
+	_puts_recursion(s + 1);
+}
+
+
+/**
+ * print_strings - check the code for Holberton School students.
+ * @separator: int
+ * @num: int
+ * @...: valist
+ *
+ * Return: Always 0.
+ */
+void print_strings(const char *separator, const unsigned int num, ...)
+{
+	va_list valist;
+	unsigned int i;
+
+	if (num == 0)
+		_putchar((char)'\n');
+
+	/* initialize valist for num number of arguments */
+	va_start(valist, num);
+
+	/* access all the arguments assigned to valist */
+	for (i = 0; i < num; i++)
+	{
+		_puts_recursion(va_arg(valist, char *));
+		if ((i != num - 1))
+		{
+			if (separator != NULL)
+			{
+				_puts_recursion((char *)separator);
+			}
+			else
+			{
+				_puts_recursion((char *)"(nil)");
+			}
+		}
+		if (i == num - 1)
+		{
+			_putchar((char)'\n');
+		}
+	}
+
+	/* clean memory reserved for valist */
+	va_end(valist);
+}
