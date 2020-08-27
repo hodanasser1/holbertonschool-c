03d923cf19409d170827985343f62ed5386d349b
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-27 19:15:11 +0000

    0

--- 0x0A-malloc_free/0-create_array.c
+++ 0x0A-malloc_free/0-create_array.c
@@ -19,7 +19,7 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 	}
 	arr = (char *) malloc(sizeof(char) *  size);
-	for (i = 0; i < size; i++)
+	for (i = 0; arr != NULL && i < size; i++)
 	{
 		arr[i] = c;
 	}
