6ee710b543bd6263938db9387955f83e01ef135d
hodanasser1 <hoda.nasser933@gmail.com>
2020-08-28 20:12:19 +0000

    first commit

--- 0x0A-malloc_free/5-argstostr.c
+++ 0x0A-malloc_free/5-argstostr.c
@@ -1,15 +1,23 @@
 #include "holberton.h"
-
 #include <stdio.h>
 #include <unistd.h>
+
+/*
+* argstostr - prints a grid of integers
+* @av: the address of the two dimensional grid
+* @ac: height of the grid
+*
+* Return: Nothing.
+*/
 
 char *argstostr(int ac, char **av)
 {
 
-  int i;
-  for (i = 0; i < ac; i++)
-  {
-     printf("%s\n", av[i]);
-  }
-  return 0;
+	int i;
+
+	for (i = 0; i < ac; i++)
+{
+	printf("%s\n", av[i]);
+}
+	return (0);
 }
