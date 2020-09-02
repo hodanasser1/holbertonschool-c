700954823bf97da970bea76ee3c9a5f25e97dff5
hodanasser1 <hoda.nasser933@gmail.com>
2020-09-02 12:58:14 +0000

    test

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,19 +1,15 @@
-#ifndef HOLBERTON
-#define HOLBERTON
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdint.h>
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
 
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int check_num(char *st)
-unsigned int string_length(char *str)
-void print_string(char *st)
-void *_calloc(unsigned int number, unsigned int size)
-int main(int argc, char **argv)
+int check_number(char *str);
+unsigned int string_length(char *str);
+void print_string(char *str);
+void *_calloc(unsigned int number, unsigned int size);
 int _putchar(char c);
 
 #endif
