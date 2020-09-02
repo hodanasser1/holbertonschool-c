e2ee87b359978f29484267a4e14cd9c51676ce0c
hodanasser1 <hoda.nasser933@gmail.com>
2020-09-02 12:30:41 +0000

    test

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -9,5 +9,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int check_num(char *st)
+unsigned int string_length(char *str)
+void print_string(char *st)
+void *_calloc(unsigned int number, unsigned int size)
+int main(int argc, char **argv)
 
 #endif
